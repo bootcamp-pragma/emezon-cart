b358fc7dc3d11f88fa6db9557a559f69e458a6c4
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-11-04 04:23:51 -0500

    Created RestApiConstants

---
+++ src/main/java/com/emezon/cart/infra/inbound/rest/constants/RestApiConstants.java
@@ -0,0 +1,11 @@
+package com.emezon.cart.infra.inbound.rest.constants;
+
+public class RestApiConstants {
+
+    public static final String API_BASE = "/api/v1";
+    public static final String API_CART = API_BASE + "/cart";
+    public static final String API_FEIGN_USER = API_BASE + "/user";
+
+    private RestApiConstants() { }
+
+}
