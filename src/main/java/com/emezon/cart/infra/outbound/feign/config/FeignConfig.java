32b9dcacb343adca1b2139da477750ff8a2e15ec
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-11-04 04:22:41 -0500

    Addec IUserFeignClient

---
+++ src/main/java/com/emezon/cart/infra/outbound/feign/config/FeignConfig.java
@@ -0,0 +1,27 @@
+package com.emezon.cart.infra.outbound.feign.config;
+
+import com.emezon.stock.domain.spi.IJwtHolder;
+import com.emezon.stock.infra.outbound.feign.utils.CustomErrorDecoder;
+import com.emezon.stock.infra.outbound.feign.utils.FeignClientInterceptor;
+import feign.codec.ErrorDecoder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class FeignConfig {
+
+    private final IJwtHolder jwtHolder;
+
+    @Bean
+    public ErrorDecoder errorDecoder(){
+        return new CustomErrorDecoder();
+    }
+
+    @Bean
+    public FeignClientInterceptor feignClientInterceptor() {
+        return new FeignClientInterceptor(jwtHolder);
+    }
+
+}
