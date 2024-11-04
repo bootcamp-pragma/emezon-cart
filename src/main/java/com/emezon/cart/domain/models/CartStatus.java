f2e556ad9149ce56f991bf9b6ab8dba75023f422
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-11-04 13:32:47 -0500

    Created CartStatus

---
+++ src/main/java/com/emezon/cart/domain/models/CartStatus.java
@@ -0,0 +1,24 @@
+package com.emezon.cart.domain.models;
+
+public enum CartStatus {
+
+    ACTIVE(1),
+    COMPLETED(2),
+    CANCELED(3);
+
+    private final int status;
+
+    CartStatus(int status) {
+        this.status = status;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(status);
+    }
+
+}
