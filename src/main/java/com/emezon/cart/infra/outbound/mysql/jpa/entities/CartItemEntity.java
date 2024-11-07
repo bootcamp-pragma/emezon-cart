da216895e3f5c5284d50f66e58267518a6647e13
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-11-07 03:43:23 -0500

    Updated CartEntity and added CartItemEntity

---
+++ src/main/java/com/emezon/cart/infra/outbound/mysql/jpa/entities/CartItemEntity.java
@@ -0,0 +1,42 @@
+package com.emezon.cart.infra.outbound.mysql.jpa.entities;
+
+import com.emezon.cart.infra.outbound.mysql.jpa.constants.CartItemEntityConstants;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDateTime;
+
+@Entity(name = CartItemEntityConstants.ENTITY_NAME)
+@Table(name = CartItemEntityConstants.TABLE_NAME)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItemEntity {
+
+    @Id
+    @UuidGenerator
+    private String id;
+
+    @ManyToOne
+    private CartEntity cart;
+
+    @Column(nullable = false)
+    private String articleId;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
