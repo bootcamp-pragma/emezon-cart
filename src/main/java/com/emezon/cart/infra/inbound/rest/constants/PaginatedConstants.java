54bfacc834302fca5c510d060583d5de51025139
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-11-10 09:42:32 -0500

    Created CartItemController

---
+++ src/main/java/com/emezon/cart/infra/inbound/rest/constants/PaginatedConstants.java
@@ -0,0 +1,19 @@
+package com.emezon.cart.infra.inbound.rest.constants;
+
+public class PaginatedConstants {
+
+    public static final String PARAM_PAGE = "page";
+    public static final String PARAM_PAGE_DESC = "Número de la página";
+    public static final String PARAM_PAGE_EXAMPLE = "0";
+
+    public static final String PARAM_SIZE = "size";
+    public static final String PARAM_SIZE_DESC = "Tamaño de la página";
+    public static final String PARAM_SIZE_EXAMPLE = "10";
+
+    public static final String PARAM_SORT = "sort";
+    public static final String PARAM_SORT_DESC = "Criterios de ordenamiento (puede incluir múltiples valores)";
+    public static final String PARAM_SORT_EXAMPLE = "name,asc";
+
+    private PaginatedConstants() { }
+
+}
---
+++ src/main/java/com/emezon/cart/infra/inbound/rest/controllers/CartItemController.java
@@ -0,0 +1,50 @@
+package com.emezon.cart.infra.inbound.rest.controllers;
+
+import com.emezon.cart.app.dtos.CartItemDTO;
+import com.emezon.cart.app.dtos.CreateCartItemDTO;
+import com.emezon.cart.app.handlers.ICartItemHandler;
+import com.emezon.cart.domain.utils.PaginatedResponse;
+import com.emezon.cart.infra.inbound.rest.constants.PaginatedConstants;
+import com.emezon.cart.infra.inbound.rest.constants.RestApiConstants;
+import com.emezon.cart.infra.inbound.rest.utils.ValidPageableRequest;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(RestApiConstants.API_CART_ITEM)
+@RequiredArgsConstructor
+public class CartItemController {
+
+    private final ICartItemHandler cartItemHandler;
+
+    @GetMapping
+    @Parameters({
+            @Parameter(name = PaginatedConstants.PARAM_PAGE, description = PaginatedConstants.PARAM_PAGE_DESC, example = PaginatedConstants.PARAM_PAGE_EXAMPLE),
+            @Parameter(name = PaginatedConstants.PARAM_SIZE, description = PaginatedConstants.PARAM_SIZE_DESC, example = PaginatedConstants.PARAM_SIZE_EXAMPLE),
+            @Parameter(name = PaginatedConstants.PARAM_SORT, description = PaginatedConstants.PARAM_SORT_DESC,
+                    array = @ArraySchema(schema = @Schema(type = "string", example = PaginatedConstants.PARAM_SORT_EXAMPLE)))
+    })
+    public ResponseEntity<PaginatedResponse<CartItemDTO>> getAllCartItems(
+            @Parameter(hidden = true) @RequestParam @ValidPageableRequest(target = CartItemDTO.class) @Valid
+            MultiValueMap<String, String> queryParams
+    ) {
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<CartItemDTO> createCartItem(@RequestBody @Valid CreateCartItemDTO createCartItemDTO) {
+        URI location = URI.create(RestApiConstants.API_CART_ITEM);
+        CartItemDTO createdCartItem = cartItemHandler.createCartItem(createCartItemDTO);
+        return ResponseEntity.created(location).body(createdCartItem);
+    }
+
+}
